cb4872cf0a6a26515ecc4598d0bcf85ddaea6d78
Kauan Souza Felicissimo <kauanfelicissimo@gmail.com>
2025-05-07 11:55:03 -0300

    footer

--- header/header.php
+++ header/header.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link rel="stylehsheet" href="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php include 'header/header.php';?>
+
+
+
+<?php include 'footer/footer.php';?>
