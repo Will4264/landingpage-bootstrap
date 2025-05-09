234a3af76e065c5458fc9828cd69d20e281dc367
Kauan Souza Felicissimo <kauanfelicissimo@gmail.com>
2025-05-09 09:16:28 -0300

    footer e css

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php include 'header/header.php';?>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="titulo">Bem-vindo ao nosso site!</h1>
+                <p class="oferta">Frete Grátis na primeira compra!</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+            
+                <img src="img/98456451A9.avif" alt="Tênis 2"
+                width="200" height="200">
+                <h4 class="tenis">Tenis Addidas Branco</h4>
+                <p class="texto"> Casual </p>
+                <p class="texto"> R$ 299,99</p>
+            </div>
+            <div class="col-md-4">
+            <img src="img/058880IDA1.png" alt="Tênis 3"
+            width="200" height="200">
+                <h4 class="tenis">Tênis Nike Ciurt Vision Low V3 Masculino</h4>
+                <p class="texto"> Casual </p>
+                <p class="texto">R$569,99</p>
+            </div>
+            <div class="col-md-4">
+            <img src="img/tenis-capricho-azul-anil-4-600x600.jpg" alt="Tênis 1"
+            width="200" height="200">
+                <h4 class="tenis">Tenis Feminino Casual Capricho Like Class Anil/Cobre</h4>
+                <p class="texto">Casual</p>
+                <p class="texto">R$569,99</p>
+            </div>
+            <div class="col-md-4">
+            
+                <img src="img/colorado-roxo-1.png" alt="Tênis 2"
+                width="200" height="200">
+                <h4 class="tenis">Tenis Oldsen Feminino Colorado - Roxo</h4>
+                <p class="texto"> Casual </p>
+                <p class="texto"> R$ 299,99</p>
+            </div>
+            <div class="col-md-4" >
+            
+                <img  id="sonic" src="img/sonic.PNG" alt="Tênis 2"
+                width="230" height="200">
+                <h4 class="tenis">Tenis do Sonic oficial Nike</h4>
+                <p class="texto"> Casual </p>
+                <p class="texto"> R$ 799,99</p>
+            </div>
+
+            <div class="col-md-4">
+            
+                <img src="img/Jordan.png" alt="Tênis 2"
+                width="200" height="200">
+                <h4 class="tenis">Nike Jordan Chicago Dunk Low</h4>
+                <p class="texto"> Casual </p>
+                <p class="texto"> R$ 3.599,99</p>
+            </div>
+        </div>
+   
+    
+    </div>
+
+        
+</main>
+
+
+<?php include 'footer/footer.php';?>
